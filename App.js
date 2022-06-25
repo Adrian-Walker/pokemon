234b4554854ef00b6dad8e230acb477d3fe05647
Adrian Walker <adrianwalker@MacBook-Pro.local>
2022-06-24 23:13:24 -0400

    nba players

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState, useCallback } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const path = "https://api-nba-v1.p.rapidapi.com/players";
 
 export default function App() {
+  useEffect(async () => {
+    const fetchPlayers = async () => {
+      const response = await fetch(path);
+      return response;
+    };
+    fetchPlayers();
+    response = await response.json();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>FUCK YOU</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
